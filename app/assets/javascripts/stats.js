6fbf91ddbcbb3eef7758d49f14b2db767b7a1926
Bruno Gonçales <bgl.bruno@gmail.com>
2015-07-13 00:33:08 -0300

    Add some labels and controls to poll view stats section

---
+++ app/assets/javascripts/stats.js
@@ -0,0 +1,24 @@
+$(function() {
+  $('.chart.yes').easyPieChart({
+    barColor: '#8bc34a',
+    lineWidth: 5,
+    scaleLength: 10
+  });
+  $('.chart.no').easyPieChart({
+    barColor: '#ff5252',
+    lineWidth: 5,
+    scaleLength: 10
+  });
+
+  $('.btn_stats').on('click', function(){
+    var $this = $(this);
+    var $p = $this.parent().find('.stats');
+    if( $p.hasClass('expanded') ) {
+      $p.removeClass('expanded').slideUp();
+    }else{
+      $('.stats').removeClass('expanded').slideUp();
+      $p.addClass('expanded').slideDown();
+    }
+  });
+
+});
